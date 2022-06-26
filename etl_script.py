a31fc5c2bb77b868239db12c15890815f93044d6
kaiying <kaiying2205@gmail.com>
2022-06-26 18:08:57 +0800

    creation of df1 (desired schema 1)

--- etl_script.py
+++ etl_script.py
@@ -21,5 +21,12 @@ for row in couch_values["rows"]:
 
 # convert values to dataframe and convert timestamp column to datetime column type
 couchdb_table = pd.DataFrame(couch_subset, columns=["_id", "_rev", "event_type", "user_id", "timestamp"])
-couchdb_table["timestamp"] = couchdb_table["timestamp"].apply(pd.to_datetime)
+# modify couchdb_table to remove timezone in timestamp column
+# couchdb_table["timestamp"] = couchdb_table["timestamp"].apply(pd.to_datetime)
+couchdb_table["timestamp"] = pd.to_datetime(couchdb_table["timestamp"]).dt.tz_localize(None)
 
+# df1: desired schema 1
+df1 = couchdb_table.merge(location_table, on=['user_id','timestamp'], how='left')
+df1 = df1.drop(columns='_rev')
+df1 = df1[['_id', 'user_id', 'event_type', 'location', 'timestamp']]
+df1 = df1.rename(columns={"_id":"id"})
