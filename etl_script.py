e7586c9e00ce10de1eee4ae1ad39bb43d9b0b483
kaiying <kaiying2205@gmail.com>
2022-06-27 11:17:50 +0800

    creation of df2 (desired schema 2)

--- etl_script.py
+++ etl_script.py
@@ -30,3 +30,28 @@ df1 = couchdb_table.merge(location_table, on=['user_id','timestamp'], how='left'
 df1 = df1.drop(columns='_rev')
 df1 = df1[['_id', 'user_id', 'event_type', 'location', 'timestamp']]
 df1 = df1.rename(columns={"_id":"id"})
+
+# df2: desired schema 2
+
+# sort location_table according to each individual user_id and timestamp
+df2 = location_table.sort_values(by=['user_id','timestamp'])
+
+# create date column by extracting date from timestamp
+df2['date'] = pd.to_datetime(df2['timestamp']).dt.date
+
+# create time_spent column by converting timestamp to minutes format and getting the difference from previous record for same user
+df2['timestamp'] = pd.to_datetime(df2['timestamp'])
+df2['timestamp'] = (df2['timestamp'] - df2['timestamp'].iloc[0])/pd.to_timedelta('1Min')
+df2['time_spent'] = list(df2.groupby(df2.user_id).apply(lambda x: x['timestamp']-x['timestamp'].shift()))
+df2['time_spent'] = df2.groupby(['user_id', 'location'])['time_spent'].transform('sum')
+
+# create visit_count column
+df2['visit_count'] = df2.groupby(['user_id', 'location'])['location'].transform('count')
+
+# tidy df2 by dropping unrequired columns, arranging columns, dropping duplicates and sort according to user_id and location
+df2 = df2.drop(columns='timestamp')
+df2 = df2[['date', 'user_id', 'location', 'time_spent', 'visit_count']]
+df2 = df2.drop_duplicates()
+df2 = df2.sort_values(by=['user_id','location']).reset_index(drop=True)
+
+print(df2)
